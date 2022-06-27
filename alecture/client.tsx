62e1177947293a65a9e8eb84b3f1b67164d1d674
jeongjieun <assd3327@gmail.com>
2022-06-27 11:36:35 +0900

    Fix bug

--- alecture/client.tsx
+++ alecture/client.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import {render} from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 
-import App from './layouts./App';
+import App from "./layouts/App";
 
-render(<App />, document.querySelector(selectors: '#app'));
+render(<App />, document.querySelector("#app"));
