05c5f272ab4d03052bf9d32b9f0c9423369a9558
jeongjieun <assd3327@gmail.com>
2022-07-28 18:42:33 +0900

    Update chat to apply regular expression to make the content look good

--- alecture/components/Chat/index.tsx
+++ alecture/components/Chat/index.tsx
@@ -1,15 +1,52 @@
-import React, { VFC } from "react";
+import React, { VFC, memo, useMemo } from "react";
 import { ChatWrapper } from "@components/Chat/styles";
 import gravatar from "gravatar";
 import { IDM } from "@typings/db";
 import dayjs from "dayjs";
-
+import regexifyString from "regexify-string";
+import { Workspaces } from "@layouts/Workspace/styles";
+import { Link, useParams } from "react-router-dom";
 interface Props {
   data: IDM;
 }
 
 const Chat: VFC<Props> = ({ data }) => {
+  const { workspace } = useParams<{ workspace: string }>();
   const user = data.Sender;
+  // @[hihi](1)
+  // .은 모든 글자
+  // .+는 모든 글자를 한 개 이상
+  // ?는 0개나 1개
+  // *는 0개 이상
+  // \d는 숫자를 의미
+  // g는 모두찾기
+  // @[hihi!](1)에서 []안에 예시
+  // [+]는 최대한 많이 => hihi!
+  // [+?]는 최대한 조금 => hihi
+  // |는 또는을 의미, \n는 줄바꿈 의미
+  // ()로 묶는 건 그루핑으로, 묶인 값이 arr[1], arr[2], ...에 추가됨
+
+  // memo는 말단컴포넌트 캐싱할 때
+  // useMemo는 hooks에서 캐싱하고 싶은 함수, 부모컴포넌트가 렌더되더라도 자식컴포넌트는 렌더될 필요 없을 때
+  const result = useMemo(
+    () =>
+      regexifyString({
+        input: data.content,
+        pattern: /@\[(.+?)]\((\d+?)\)|\n/g, // id나 줄바꿈 둘 다 찾았을 때
+        decorator(match, index) {
+          const arr = match.match(/@\[(.+?)]\((\d+?)\)/)!; // id만 찾았을 때
+          if (arr) {
+            return (
+              <Link key={match + index} to={`/workspace/${workspace}/dm/${arr[2]}`}>
+                @{arr[1]}
+              </Link>
+            );
+          }
+          return <br key={index} />; // 나머지(줄바꿈) 찾았을때
+        },
+      }),
+    [data.content],
+  );
   return (
     <ChatWrapper>
       <div className="chat-img">
@@ -20,10 +57,10 @@ const Chat: VFC<Props> = ({ data }) => {
           <b>{user.nickname}</b>
           <span> {dayjs(data.createdAt).format("h:mm:A")}</span>
         </div>
-        <p>{data.content}</p>
+        <p>{result}</p>
       </div>
     </ChatWrapper>
   );
 };
 
-export default Chat;
+export default memo(Chat);
--- alecture/components/Chat/styles.tsx
+++ alecture/components/Chat/styles.tsx
@@ -3,7 +3,10 @@ import styled from "@emotion/styled";
 export const ChatWrapper = styled.div`
   display: flex;
   padding: 8px 20px;
+  // a?.b; // optional chaning
+  // a??b; // nullish coalescing
   &:hover {
+    // nested selector
     background: #eee;
   }
   & .chat-img {
