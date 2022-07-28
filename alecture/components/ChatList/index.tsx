077319c4f19d4b4b17b71bd52a8c1dc24a995400
jeongjieun <assd3327@gmail.com>
2022-07-28 17:30:26 +0900

    Add scrollbar

--- alecture/components/ChatList/index.tsx
+++ alecture/components/ChatList/index.tsx
@@ -1,7 +1,8 @@
-import React, { VFC } from "react";
+import React, { useCallback, useRef, VFC } from "react";
 import { ChatZone, Section } from "@components/ChatList/styles";
 import { IDM } from "@typings/db";
 import Chat from "@components/Chat";
+import { Scrollbars } from "react-custom-scrollbars";
 
 interface Props {
   chatData?: IDM[];
@@ -10,11 +11,15 @@ interface Props {
 // Channel, DM 함께 사용됨
 const ChatList: VFC<Props> = ({ chatData }: Props) => {
   console.log("chatData", chatData);
+  const scrollRef = useRef(null);
+  const onScroll = useCallback(() => {}, []);
   return (
     <ChatZone>
-      {chatData?.map((chat: IDM) => (
-        <Chat key={chat.id} data={chat} />
-      ))}
+      <Scrollbars autoHide ref={scrollRef} onScrollFrame={onScroll}>
+        {chatData?.map((chat: IDM) => (
+          <Chat key={chat.id} data={chat} />
+        ))}
+      </Scrollbars>
     </ChatZone>
   );
 };
